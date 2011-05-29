411fbbae232ec63cea7bf704409f02b7383411d2
Tim <Tim@elizabeth-swanwicks-imac.local>
2011-05-30 09:05:26 +1000

    Initial Commit

---
+++ GraphingCalculator/GraphingCalculatorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GraphingCalculatorAppDelegate.h
+//  GraphingCalculator
+//
+//  Created by Tim on 30/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphingCalculatorAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
